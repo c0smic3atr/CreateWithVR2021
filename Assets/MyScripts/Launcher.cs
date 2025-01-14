5d16d327ea26839dc931e6487a0034093d096d36
c0smic3atr <marsdatrandomdemon@gmail.com>
2025-01-14 08:06:23 -0800

    potato head - create w code day 4

---
+++ Assets/MyScripts/Launcher.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject spawnPoint;
+
+    public void Shoot()
+    {
+        Instantiate(projectile, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+}
---
+++ Assets/MyScripts/PhysicsProjectile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsProjectile : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float shootForce;
+    public float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * shootForce, ForceMode.Impulse);
+        Destroy(gameObject, duration);
+    }
+
+    
+}
