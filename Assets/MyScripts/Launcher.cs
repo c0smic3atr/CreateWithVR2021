8c8cfabdebc99ec203f553765dfd4e28acf154b4
c0smic3atr <marsdatrandomdemon@gmail.com>
2025-01-14 10:13:55 -0800

    day 5

--- Assets/MyScripts/Launcher.cs
+++ Assets/MyScripts/Launcher.cs
@@ -6,9 +6,11 @@ public class Launcher : MonoBehaviour
 {
     public GameObject projectile;
     public GameObject spawnPoint;
+    public TvCanvasControl tvControl;
 
     public void Shoot()
     {
         Instantiate(projectile, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        tvControl.AddToScore();
     }
 }
