7ba49308d4df5f1d68836d06793a8e15bb64b913
c0smic3atr <marsdatrandomdemon@gmail.com>
2025-01-17 10:14:30 -0800

    vr summative 3

---
+++ Assets/MyScripts/TreeDestroyer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeDestroyer : MonoBehaviour
+{
+    public AudioSource aS;
+    public AudioClip breakSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Axe"))
+        {
+            aS.PlayOneShot(breakSound);
+            Destroy(gameObject);
+        }
+    }
+}
